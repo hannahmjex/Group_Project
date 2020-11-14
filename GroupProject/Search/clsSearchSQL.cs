3bb41002d0f431fb39f86997dc89856ead8795b0
Erik Helton <erikhelton@mail.weber.edu>
2020-11-13 20:29:18 -0700

    Added SQL methods to clsSearchSQL.cs

--- GroupProject/Search/clsSearchSQL.cs
+++ GroupProject/Search/clsSearchSQL.cs
@@ -170,5 +170,59 @@ namespace GroupProject.Search
         {
             return $"SELECT * FROM Invoices WHERE InvoicNum = {invoiceNum}";
         }
+
+        /// <summary>
+        /// Get invoices by filtered by invoice number and invoice date
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <param name="invoiceDate"></param>
+        /// <returns></returns>
+        public string GetInvoicesByInvoiceNumAndInvoiceDate(string invoiceNum, string invoiceDate)
+        {
+            return $"SELECT * FROM Invoices WHERE InvoiceNum = {invoiceDate} AND InvoiceDate = {invoiceDate}";
+        }
+
+        /// <summary>
+        /// Get invoices filtered by invoice number, invoice date, and total cost.
+        /// </summary>
+        /// <param name="invoiceNum"></param>
+        /// <param name="invoiceDate"></param>
+        /// <param name="totalCost"></param>
+        /// <returns></returns>
+        public string GetInvoicesByInvoiceNumInvoiceDateAndTotalCost(string invoiceNum, string invoiceDate, string totalCost)
+        {
+            return $"SELECT * FROM Invoices WHERE InvoiceNum = {invoiceNum} AND InvoiceDate = {invoiceDate} AND TotalCost = {totalCost}";
+        }
+
+        /// <summary>
+        /// Get invoices filtered by total cost
+        /// </summary>
+        /// <param name="totalCost"></param>
+        /// <returns></returns>
+        public string GetInvoicesByTotalCost(string totalCost)
+        {
+            return $"SELECT * FROM Invoices WHERE TotalCost = {totalCost}";
+        }
+
+        /// <summary>
+        /// Get invoices filtered by total cost and invoice date
+        /// </summary>
+        /// <param name="totalCost"></param>
+        /// <param name="invoiceDate"></param>
+        /// <returns></returns>
+        public string GetInvoicesByTotalCostAndInvoiceDate(string totalCost, string invoiceDate)
+        {
+            return $"SELECT * FROM Invoices WHERE TotalCost = {totalCost} and InvoiceDate = {invoiceDate}";
+        }
+
+        /// <summary>
+        /// Get invoices filtered by invoice date
+        /// </summary>
+        /// <param name="invoiceDate"></param>
+        /// <returns></returns>
+        public string GetInvoicesByInvoiceDate(string invoiceDate)
+        {
+            return $"SELECT * FROM Invoices WHERE InvoiceDate = {invoiceDate}";
+        }
     }
 }
