ef0e4fbdda6971ca9ed78bfa44f50690655115e0
hannahmjex <hannahmjex@gmail.com>
2020-12-05 09:44:54 -0700

    Some code clean up

--- GroupProject/Items/clsItemsSQL.cs
+++ GroupProject/Items/clsItemsSQL.cs
@@ -12,248 +12,218 @@ using System.Threading.Tasks;
 
 namespace GroupProject.Items
 {
-    public class clsItemsSQL
-    {
-        /// <summary>
-        /// Values to be returned
-        /// </summary>
-        int returnValues;
+	public class clsItemsSQL
+	{
+		/// <summary>
+		/// Values to be returned
+		/// </summary>
+		int returnValues;
 
-        /// <summary>
-        /// DataAccess Object Declaration
-        /// </summary>
-        private clsDataAccess db;
+		/// <summary>
+		/// DataAccess Object Declaration
+		/// </summary>
+		private clsDataAccess db;
 
-        /// <summary>
-        /// DataSet Object Declaration
-        /// </summary>
-        DataSet ds;
+		/// <summary>
+		/// DataSet Object Declaration
+		/// </summary>
+		DataSet ds;
 
-        /// <summary>
-        /// class item
-        /// </summary>
-        Item Item;
+		/// <summary>
+		/// class item
+		/// </summary>
+		Item Item;
 
-        /// <summary>
-        /// Default Constructor
-        /// </summary>
-        public clsItemsSQL()
-        {
-            try
-            {
-                db = new clsDataAccess();
-                ds = new DataSet();
-                Item = new Item();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
-
-        /// <summary>
-        /// Retrieves all Items 
-        /// </summary>
-        /// <returns>An ObservableCollection of all Items</returns>
-        public ObservableCollection<Item> GetAllItems()
-        {
-            try
-            {
-                ObservableCollection<Item> items = new ObservableCollection<Item>();
-                ds = db.ExecuteSQLStatement("SELECT ItemCode, ItemDesc, Cost FROM ItemDesc", ref returnValues);
-
-
-                // Iterate over rows
-                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                {
-                    // Grab data from their columns for each row
-                    string itemCode = ds.Tables[0].Rows[i][0].ToString();
-                    string itemDescription = ds.Tables[0].Rows[i][1].ToString();
-                    string itemCost = ds.Tables[0].Rows[i][2].ToString();
-
-                    // Create new local item object
-                    Item item = new Item(itemCode, itemDescription, itemCost);
-
-                    // Add newly created item object to local collection of items.
-                    items.Add(item);
-                }
-                /*
-                foreach (DataRow dr in ds.Tables[0].Rows)
-                {
-                    items.Add(new Item(Convert.ToChar(dr[0]), dr[1].ToString(), Convert.ToInt32(dr[2])));
-                }*/
-
-                return items;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
-        
-        /// <summary>
-        /// Checks if the provided Item Code does not exist
-        /// </summary>
-        /// <param name="Code">Item's Code</param>
-        /// <returns>boolean representing uniqueness of the code</returns>
-        public DataSet CheckUniqueCode(string Code)
-        {
-            try
-            {
-                
-               string sql = "SELECT ItemCode FROM ItemDesc WHERE ItemCode = @Code";
-                //string sql = "SELECT ItemCode FROM ItemDesc WHERE ItemCode = " + Code;
-
-                //Cmd = new OleDbCommand("SELECT ItemCode FROM ItemDesc WHERE ItemCode = @Code");
-                //Cmd.Parameters.Add("@Code", OleDbType.WChar).Value = Code;
-
-                //return db.ExecuteScalarSQL(Cmd) == "";
-                //string code = db.ExecuteScalarSQL(sql, (OleDbCommand cmd) => { cmd.Parameters.AddWithValue("@Code", Code); });
-                return db.ExecuteSQLStatement(sql, ref returnValues, (OleDbCommand cmd) =>
-                {
-                    cmd.Parameters.AddWithValue("@Code", Code);
-                });
-                 
-                //Do logic for boolean in itemLogic
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public clsItemsSQL()
+		{
+			try
+			{
+				db = new clsDataAccess();
+				ds = new DataSet();
+				Item = new Item();
 			}
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                                                       MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+					MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
 
-        /// <summary>
-        /// Inserts a new Item
-        /// </summary>
-        /// <param name="Code">Item's Code</param>
-        /// <param name="Desc">Item's Description</param>
-        /// <param name="Cost">Item's Cost</param>
-        public void InsertNewItem(string Code, string Desc, string Cost)
-        {
-            try
-            {
-                string sql = @"INSERT INTO ItemDesc(ItemCode, ItemDesc, Cost) VALUES ('"+Code+"', '"+Desc+"', '"+Cost+"')";
-                //Cmd = new OleDbCommand("INSERT INTO ItemDesc(ItemCode, ItemDesc, Cost) VALUES (@Code, @Desc, @Cost)");
-                //Cmd.Parameters.Add("@Code", OleDbType.WChar).Value = Code;
-                //Cmd.Parameters.Add("@Desc", OleDbType.WChar).Value = Desc;
-                //Cmd.Parameters.Add("@Cost", OleDbType.Integer).Value = Cost;
+		/// <summary>
+		/// Retrieves all Items 
+		/// </summary>
+		/// <returns>An ObservableCollection of all Items</returns>
+		public ObservableCollection<Item> GetAllItems()
+		{
+			try
+			{
+				ObservableCollection<Item> items = new ObservableCollection<Item>();
+				ds = db.ExecuteSQLStatement("SELECT ItemCode, ItemDesc, Cost FROM ItemDesc", ref returnValues);
 
-                //db.ExecuteNonQuery(Cmd);
+				// Iterate over rows
+				for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+				{
+					// Grab data from their columns for each row
+					string itemCode = ds.Tables[0].Rows[i][0].ToString();
+					string itemDescription = ds.Tables[0].Rows[i][1].ToString();
+					string itemCost = ds.Tables[0].Rows[i][2].ToString();
 
-                db.ExecuteNonQuery(sql);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
+					// Create new local item object
+					Item item = new Item(itemCode, itemDescription, itemCost);
 
-        /// <summary>
-        /// Deletes an Item 
-        /// </summary>
-        /// <param name="Code">Item to delete's code</param>
-        public void DeleteItem(string Code)
-        {
-            try
-            {
-                string sql = "DELETE FROM ItemDesc WHERE ItemCode = '"+Code+"'";
+					// Add newly created item object to local collection of items.
+					items.Add(item);
+				}
+				return items;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+					MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
 
-               // Cmd = new OleDbCommand("DELETE FROM ItemDesc WHERE ItemCode = @Code");
-                //Cmd.Parameters.Add("@Code", OleDbType.WChar).Value = Code;
+		/// <summary>
+		/// Checks if the provided Item Code does not exist
+		/// </summary>
+		/// <param name="Code">Item's Code</param>
+		/// <returns>boolean representing uniqueness of the code</returns>
+		public DataSet CheckUniqueCode(string Code)
+		{
+			try
+			{
+				string sql = "SELECT ItemCode FROM ItemDesc WHERE ItemCode = @Code";
+				return db.ExecuteSQLStatement(sql, ref returnValues, (OleDbCommand cmd) =>
+				{
+					cmd.Parameters.AddWithValue("@Code", Code);
+				});
 
-                db.ExecuteNonQuery(sql);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
+				//Do logic for boolean in itemLogic
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+													   MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
 
-        /// <summary>
-        /// Updates an Item
-        /// </summary>
-        /// <param name="Code">Item's Code</param>
-        /// <param name="Desc">Item's Description</param>
-        /// <param name="Cost">Item's Cost</param>
-        public void UpdateItem(string Code, string Desc, string Cost)
-        {
-            try
-            {
-                //string sql = @"UPDATE ItemDesc SET ItemDesc = '"+Desc+"' AND Cost = '"+Cost+"' AND ItemCode = '"+Code.ToString()+"' WHERE ItemCode = '"+Code.ToString()+"'";
+		/// <summary>
+		/// Inserts a new Item
+		/// </summary>
+		/// <param name="Code">Item's Code</param>
+		/// <param name="Desc">Item's Description</param>
+		/// <param name="Cost">Item's Cost</param>
+		public void InsertNewItem(string Code, string Desc, string Cost)
+		{
+			try
+			{
+				string sql = @"INSERT INTO ItemDesc(ItemCode, ItemDesc, Cost) VALUES ('" + Code + "', '" + Desc + "', '" + Cost + "')";
+				db.ExecuteNonQuery(sql);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+					MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
 
-                var code = Code.ToString();
+		/// <summary>
+		/// Deletes an Item 
+		/// </summary>
+		/// <param name="Code">Item to delete's code</param>
+		public void DeleteItem(string Code)
+		{
+			try
+			{
+				string sql = "DELETE FROM ItemDesc WHERE ItemCode = '" + Code + "'";
+				db.ExecuteNonQuery(sql);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+					MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
 
-                var sql = @"UPDATE ItemDesc SET ItemDesc = '" + Desc + "', Cost = '" + Cost + "' WHERE ItemCode = '" + Code + "'";
-                // Cmd = new OleDbCommand("UPDATE ItemDesc SET ");
+		/// <summary>
+		/// Updates an Item
+		/// </summary>
+		/// <param name="Code">Item's Code</param>
+		/// <param name="Desc">Item's Description</param>
+		/// <param name="Cost">Item's Cost</param>
+		public void UpdateItem(string Code, string Desc, string Cost)
+		{
+			try
+			{
+				//string sql = @"UPDATE ItemDesc SET ItemDesc = '"+Desc+"' AND Cost = '"+Cost+"' AND ItemCode = '"+Code.ToString()+"' WHERE ItemCode = '"+Code.ToString()+"'";
 
-                ////If a new description is provided then add it to the SET clause
-                //if (Desc != null)
-                //{
-                //    Cmd.CommandText += "ItemDesc = '" + Desc + "'";
-                //}
-                //if (Cost != null)
-                //{
-                //    //If a new cost is provided as well as desc then add a comma, otherwise just add cost to SET clause
-                //    if (Desc != null)
-                //    {
-                //        Cmd.CommandText += ", Cost = '" + Cost + "'";
-                //    }
-                //    else
-                //    {
-                //        Cmd.CommandText += "Cost = '" + Cost + "'";
-                //    }
-                //}
-                //Cmd.CommandText += " WHERE ItemCode = '" + Code + "'";
+				var code = Code.ToString();
 
-                //db.ExecuteNonQuery(Cmd);
-                db.ExecuteNonQuery(sql);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
+				var sql = @"UPDATE ItemDesc SET ItemDesc = '" + Desc + "', Cost = '" + Cost + "' WHERE ItemCode = '" + Code + "'";
+				// Cmd = new OleDbCommand("UPDATE ItemDesc SET ");
 
-        /// <summary>
-        /// Checks if an Item exists on any Invoice
-        /// </summary>
-        /// <param name="SelectedItem">Item to check for</param>
-        /// <returns>A List of Invoice Numbers on which the Item exists</returns>
-        public List<int> CheckItemOnInvoice(Item SelectedItem)
-        {
-            try
-            {
-                List<int> invoiceNums = new List<int>();
-                //Cmd = new OleDbCommand("SELECT DISTINCT InvoiceNum FROM LineItems WHERE ItemCode = @ItemCode");
-                //Cmd.Parameters.Add("@ItemCode", OleDbType.WChar).Value = SelectedItem.Code;
+				////If a new description is provided then add it to the SET clause
+				//if (Desc != null)
+				//{
+				//    Cmd.CommandText += "ItemDesc = '" + Desc + "'";
+				//}
+				//if (Cost != null)
+				//{
+				//    //If a new cost is provided as well as desc then add a comma, otherwise just add cost to SET clause
+				//    if (Desc != null)
+				//    {
+				//        Cmd.CommandText += ", Cost = '" + Cost + "'";
+				//    }
+				//    else
+				//    {
+				//        Cmd.CommandText += "Cost = '" + Cost + "'";
+				//    }
+				//}
+				//Cmd.CommandText += " WHERE ItemCode = '" + Code + "'";
 
-                var sql = "SELECT DISTINCT InvoiceNum FROM LineItems WHERE ItemCode = '"+ SelectedItem.Code+"'";
+				//db.ExecuteNonQuery(Cmd);
+				db.ExecuteNonQuery(sql);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+					MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
 
-                int retVal = 0;
+		/// <summary>
+		/// Checks if an Item exists on any Invoice
+		/// </summary>
+		/// <param name="SelectedItem">Item to check for</param>
+		/// <returns>A List of Invoice Numbers on which the Item exists</returns>
+		public List<int> CheckItemOnInvoice(Item SelectedItem)
+		{
+			try
+			{
+				List<int> invoiceNums = new List<int>();
 
-                //ds = db.ExecuteSQLStatement(Cmd, ref retVal);
+				var sql = "SELECT DISTINCT InvoiceNum FROM LineItems WHERE ItemCode = '" + SelectedItem.Code + "'";
 
-                ds = db.ExecuteSQLStatement(sql, ref retVal);
+				int retVal = 0;
 
-                foreach (DataRow dr in ds.Tables[0].Rows)
-                {
-                    invoiceNums.Add(Convert.ToInt32(dr[0]));
-                }
+				ds = db.ExecuteSQLStatement(sql, ref retVal);
 
-                return invoiceNums;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
-                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-            }
-        }
-    }
+				foreach (DataRow dr in ds.Tables[0].Rows)
+				{
+					invoiceNums.Add(Convert.ToInt32(dr[0]));
+				}
+
+				return invoiceNums;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+					MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
+	}
 }
 
